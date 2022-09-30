9fdfe5e685d45c0478772c78ee56bdac5a4a73da
maximiliano Castucci <maximiliano.castrucci@gmail.com>
2022-10-01 00:58:27 +0200

    adding meta-characters and improved engine

--- regex.py
+++ regex.py
@@ -1,3 +1,5 @@
+META_CHARS = ['?', '*', '+', '$', '^']
+
 def main():
     regex, string_to_test = get_user_inputs()
 
@@ -15,14 +17,71 @@ def print_result(result):
     print(result)
 
 
-def test_string_with_regex(regex, string_to_test):
-    """Test the string with the given regex
-    If the string is longer than the regex, the function will be called again
-    with a slice of the original string until the string is consumed or a match with the
-    regex happens.
-    It first loop and slice the string from left to right and if there is no match, it loops
-    from right to left
+def test_char(char, string_to_test, index, wildcard=False) -> tuple[int, bool]:
+    """ tests if the char matches the given string from the given index and returns the
+    index where it does not match anymore
 
+    Returns
+        the index where the String does not match the char
+    """
+
+    if wildcard and wildcard != '$':
+        # char is optional or one time
+        if wildcard == '?':
+            if char == string_to_test[index] or char == '.':
+                return (index + 1), True
+            else:
+                return index, True
+        # matches zero or more times
+        elif wildcard == '*':
+            if char == string_to_test[index] or char == '.':
+                # count the next chars if there is more
+                next_index = consume_char_n_times(char, string_to_test, index)
+                return next_index, True
+            else:
+                return index, True
+        # matches at least one time and n times
+        elif wildcard == '+':
+            if char == string_to_test[index] or char == '.':
+                next_index = consume_char_n_times(char, string_to_test, index)
+                return next_index, True
+            else:
+                return index, False
+        else:
+            # invalid case
+            return index, True
+    else:
+        # if no wildcard, the char should match the string at the index
+        if char == string_to_test[index] or char == '.':
+            return (index + 1), True
+        else:
+            return index, False
+
+
+def consume_char_n_times(char, string_to_consume, pointer=0, stop_at=None):
+    """Consumes a char n times in a string from an pointer
+
+    Returns
+        The index that points the next index that is different from the given char
+    """
+    # count the next chars if there is more
+    next_index = pointer
+    next_string_char = char
+    while next_string_char == char or char == '.' and next_string_char != stop_at:
+        try:
+            temp_index = next_index + 1
+            temp = string_to_consume[temp_index]
+        except IndexError:
+            return next_index
+        else:
+            next_index = temp_index
+            next_string_char = temp
+    return next_index
+
+
+def test_string_with_regex(regex, string_to_test):
+    """Test the string with the given regex if the string could not be matched
+    the same function is re-called with a splice of the original string
 
     Params:
         - regex - The regex to use to test the string
@@ -32,95 +91,106 @@ def test_string_with_regex(regex, string_to_test):
         - True if there is a match of the string with the Regex
         - False if there is no matches
     """
-    start_wildcard_available = '^' in regex
-    end_wildcard_available = '$' in regex
-    regex_clean = "".join(regex) # make a copy
 
-    # Check that wildcard appears only one time
-    if start_wildcard_available:
-        # this is lazzy, probably i should check for scape chars too
-        if regex.count('^') > 1:
-            return False # invalid regex
-        regex_clean = regex_clean.replace("^", "")
+    # pointer to point the current index of the string to test
+    string_pointer = 0
+    match_at_start = False
+    # Iterate over regex and check string match
+    valid = True
+    for i in range(len(regex)):
+        # check if current index in regex is a meta-char
+        if regex[i] in META_CHARS:
 
-    # Check that wildcard appears only one time
-    if end_wildcard_available:
-        # this is lazzy, probably i should check for scape chars too
-        if regex.count('$') > 1:
-            return False  # invalid regex
-        regex_clean = regex_clean.replace("$", "")
-
-    # If the length of the regex and the string to test are equal
-    # the string is tested without further process
-    if len(regex_clean) == len(string_to_test):
-        # since the string has the same length (without the wildcards)
-        # it will be true if it is at the end or at the begining
-        return test_string_slice_with_regex(regex_clean, string_to_test)
-    elif len(regex_clean) < len(string_to_test):
-        # Iterate over the slices of the string to try to match the regex.
-        if start_wildcard_available:
-            # if regex contains start string wilcard, first slice should match regex, if not
-            # we dont continue
-            result = test_string_slice_with_regex(regex_clean, string_to_test[:len(regex_clean)])
-            if not result:
-                return False
-            # if there is no other anchors / wildcards, this is a match
-            if not end_wildcard_available:
-                return True
-
-        if end_wildcard_available:
-            # if regex contaisn end wildcard, an slice from the end of the string
-            # should match the regex, if not there is not need to process further
-            result = test_string_slice_with_regex(regex_clean, string_to_test[-len(regex_clean):])
-            if not result:
-                return False
+            # if the regex char is ^ it will be valid in two situations
+            # 1. The string pointer is still at 0
+            # 2. The previous char is \n
+            if regex[i] == '^':
+                if string_pointer == 0:
+                    match_at_start = True
+                    continue
+                elif string_pointer > 0 and string_to_test[string_pointer - 1] == '\n':
+                    # if string pointer is higher than 0, it is only valid if the previous char
+                    # is a new line
+                    match_at_start = False
+                    continue
+                else:
+                    # Invalid string, fails regex
+                    valid = False
+                    break
+            # if regex char is $ means that it should be at the end of a line
+            # or end of string, next char should be \n or end of length
+            elif regex[i] == '$':
+                # string consumed only do not mistake with len -1
+                if len(string_to_test) == string_pointer:
+                    # valid case, end of the string
+                    # if valid and end of regex, the string matches
+                    if len(regex) - 1 == i and valid:
+                        break
+                elif len(string_to_test) - 1 >= string_pointer:
+                    if string_to_test[string_pointer] == '\n':
+                        string_pointer += 1
+                        # This case is only valid if next char is an end of line
+                        pass
+                    else:
+                        # invalid case, not end of line # keep slicing the string
+                        valid = test_string_with_regex(regex, string_to_test[1:])
+                        break
+            elif regex[i] == '?' or regex[i] == '*' or regex[i] == '+':
+                # if any of the wildcards is the next regex char, skip it
+                continue
+        else:
+            regex_char = regex[i]
+            # check if there is a wildcard after the current char
+            wildcard = False
+            try:
+                next_char = regex[i + 1]
+            except IndexError:
+                pass  # no next char, end of string
             else:
-                return True  # this is good enough to return a match
+                if next_char in META_CHARS:
+                    wildcard = next_char
 
-        # If nothing was returned until this point, multiple slices should be done
-        # to the string to find matches
+            if len(string_to_test) - 1 < string_pointer:
+                return False  # no string to test
 
-        left_to_right_slice = "".join(string_to_test)  # clone of the string
-        while len(left_to_right_slice) > len(regex):
-            result = test_string_with_regex(regex, left_to_right_slice[0:len(regex)])
-            if result is True:
-                return True
+            char = string_to_test[string_pointer]
+
+            if valid and regex_char == '.' and (wildcard == '+') or (wildcard == '*'):
+                # consume everything until next non-metacharacter
+                try:
+                    temp = regex[i + 2]
+                except IndexError:
+                    pass
+                else:
+                    next_regex_char_after_wildcard = temp
+                    string_pointer = consume_char_n_times(regex_char, string_to_test, string_pointer, stop_at=next_regex_char_after_wildcard)
+                    continue
+
+            if match_at_start and string_pointer == 0:
+                if char == regex[i] or regex[i] == '.':
+                    # valid case, regex matches
+                    string_pointer += 1
+                    continue
+                else:
+                    # first char does not match regex
+                    valid = False
+                    break
+            elif match_at_start and valid:
+                index, valid = test_char(regex_char, string_to_test, string_pointer, wildcard)
+                string_pointer = index
+                if not valid:
+                    # since the match should happen at the start, if there is no match
+                    # the regex test fails
+                    return False
             else:
-                left_to_right_slice = left_to_right_slice[len(regex):]
-        # If nothing was returned yet, try from right to left
-        right_to_left_slice = "".join(string_to_test)  # clone of the string
-        while len(right_to_left_slice) > len(regex):
-            result = test_string_with_regex(regex, right_to_left_slice[-len(regex):])
-            if result is True:
-                return True
-            else:
-                right_to_left_slice = right_to_left_slice[-1:-len(regex)]
-        return False  # nothing was returned before this step
-    else:
-        # this case means that the string is already consumed and a match is not
-        # possible anymore
-        return False
+                index, valid = test_char(regex_char, string_to_test, string_pointer, wildcard)
+                string_pointer = index
+                # if not valid, make a slice of the string and re-try
+                if not valid:
+                    valid = test_string_with_regex(regex, string_to_test[1:])
+                    break
 
-
-def test_string_slice_with_regex(regex, sliced_string):
-    """Tests a string slice with a regex (already filtered from starting and ending wildcards)
-
-    Returns
-        - True if the string slice matches the regex
-        - False otherwise
-    """
-    if len(regex) == len(sliced_string):
-        result = True
-        for index in range(len(regex)):
-            # If all the regex members matches the string members (or regex value is .)
-            # the string matches the regex
-            if regex[index] != '.' and sliced_string[index] != regex[index]:
-                result = False
-        return result
-    else:
-        # regex and slice do not have the same length
-        # this is a bug and should not happen
-        return False
+    return valid
 
 
 def get_user_inputs():
--- testRegex.py
+++ testRegex.py
@@ -32,25 +32,103 @@ class TestRegex(unittest.TestCase):
         self.assertEqual(rx.test_string_with_regex('ple$', 'capple'), True, f'Expected True with regex ple$ and string capple')
         self.assertEqual(rx.test_string_with_regex('rapid$', 'rapidify'), False, f'Expected False with regex rapid$ and string rapidify')
         self.assertEqual(rx.test_string_with_regex('rapid$', 'caprapid'), True, f'Expected True with regex rapid$ and string caprapid')
+        self.assertEqual(rx.test_string_with_regex('le$', 'apple'), True, f'Expected True with regex rapid$ and string caprapid')
+        self.assertEqual(rx.test_string_with_regex('.$', 'apple'), True, f'Expected True with regex rapid$ and string caprapid')
+        self.assertEqual(rx.test_string_with_regex('apple$', 'tasty apple'), True, f'Expected True with regex rapid$ and string caprapid')
+        self.assertEqual(rx.test_string_with_regex('app$', 'apple'), False, f'Expected True with regex rapid$ and string caprapid')
 
-    def hyperskill_test_cases(self):
+
+    def test_hyperskill_test_cases(self):
         """This test attempts The test cases from hyperskill"""
         test_cases = [
-            ("^app", "apple", True),
-            ("le$", "apple", True),
-            ("^a", "apple", True),
-            (".$", "apple", True),
-            ("apple$", "tasty apple", True),
-            ("^apple", "apple pie", True),
-            ("apple$", "apple", True),
-            ("^apple$", "tasty apple", False),
-            ("^apple$", "apple pie", False),
-            ("app$", "apple", False),
-            ("^le", "apple", False)
+        ("a", "a",          "True",     "Two identical patterns should return True!"),
+        ("a", "b",          "False",    "Two different patterns should not return True!"),
+        ("7", "7",          "True",     "Two identical patterns should return True!"),
+        ("6", "7",          "False",    "Two different patterns should not return True!"),
+        (".", "a",          "True",     "Don't forget that '.' is a wild-card that matches any single character."),
+        ("a", ".",          "False",    "A period in the input string is still a literal!"),
+        ("", "a",           "True",     "An empty regex always returns True!"),
+        ("", "",            "True",     "An empty regex always returns True!"),
+        ("a", "",           "False",    "A non-empty regex and an empty input string always returns False!"),
+        # stage 2
+        ("apple", "apple",  "True",     "Two identical equal-length patterns should return True!"),
+        (".pple", "apple",  "True",     "The wild-card '.' should match any single character in a string."),
+        ("appl.", "apple",  "True",     "The wild-card '.' should match any single character in a string."),
+        (".....", "apple",  "True",     "The wild-card '.' should match any single character in a string."),
+        ("", "apple",       "True",     "An empty regex always returns True!"),
+        ("apple", "",       "False",    "A non-empty regex and an empty input string always returns False!"),
+        ("apple", "peach",  "False",    "Two different patterns should not return True!"),
+        # stage 3
+        ("le", "apple",     "True",     "If the input string contains the regex, it should return True!"),
+        ("app", "apple",    "True",     "If the input string contains the regex, it should return True!"),
+        ("a", "apple",      "True",     "If the input string contains the regex, it should return True!"),
+        (".", "apple",      "True",     "Even a single wild-card character '.' can produce a match!"),
+        ("apwle", "apple",  "False",    "Two different patterns should not return True!"),
+        ("peach", "apple",  "False",    "Two different patterns should not return True!"),
+        # stage 4
+        ('^app', 'apple',           "True",
+            "A regex starting with '^' should match the following pattern only at the beginning of the input string!"),
+        ('le$', 'apple',            "True",
+            "A regex ending with '$' should match the preceding pattern only at the end of the input string!"),
+        ('^a', 'apple',             "True",
+            "A regex starting with '^' should match the following pattern only at the beginning of the input string!"),
+        ('.$', 'apple',             "True",
+            "A regex ending with '$' should match the preceding pattern only at the end of the input string!"),
+        ('apple$', 'tasty apple',   "True",
+            "A regex ending with '$' should match the preceding pattern only at the end of the input string!"),
+        ('^apple', 'apple pie',     "True",
+            "A regex starting with '^' should match the following pattern only at the beginning of the input string!"),
+        ('^apple$', 'apple',        "True",
+            "A regex starting with '^' and ending with '$' should match only the enclosed literals!"),
+        ('^apple$', 'tasty apple',  "False",
+            "A regex starting with '^' and ending with '$' should match only the enclosed literals!"),
+        ('^apple$', 'apple pie',    "False",
+            "A regex starting with '^' and ending with '$' should match only the enclosed literals!"),
+        ('app$', 'apple',           "False",
+            "A regex ending with '$' should match the preceding pattern only at the end of the input string!"),
+        ('^le', 'apple',            "False",
+            "A regex starting with '^' should match the following pattern only at the beginning of the input string!"),
+        # stage 5
+        ("colou?r", "color",        "True",
+            "'?' in a regex should match the preceding character exactly 0 or 1 times!"),
+        ("colou?r", "colour",       "True",
+            "'?' in a regex should match the preceding character exactly 0 or 1 times!"),
+        ("colou?r", "colouur",      "False",
+            "'?' in a regex should match the preceding character exactly 0 or 1 times!"),
+        ("colou*r", "color",        "True",
+            "'*' in a regex should match the preceding character 0 or more times!"),
+        ("colou*r", "colour",       "True",
+            "'*' in a regex should match the preceding character 0 or more times!"),
+        ("colou*r", "colouur",      "True",
+            "'*' in a regex should match the preceding character 0 or more times!"),
+        ("colou+r", "colour",        "True",
+             "'+' in a regex should match the preceding character 1 or more times!"),
+        ("colou+r", "color",        "False",
+            "'+' in a regex should match the preceding character 1 or more times!"),
+        (".*", "aaa",               "True",
+            "The repetition operators can be combined with the wild card '.'!"),
+        (".+", "aaa",               "True",
+            "The repetition operators can be combined with the wild card '.'!"),
+        (".?", "aaa",               "True",
+            "The repetition operators can be combined with the wild card '.'!"),
+        ("no+$", "noooooooope",     "False",
+            "The repetition operators can be combined with other metacharacters, like '.', '^' and '$'."),
+        ("^no+", "noooooooope",     "True",
+            "The repetition operators can be combined with other metacharacters, like '.', '^' and '$'."),
+        ("^no+pe$", "noooooooope",     "True",
+            "The repetition operators can be combined with other metacharacters, like '.', '^' and '$'."),
+        ("^n.+pe$", "noooooooope",     "True",
+            "The repetition operators can be combined with other metacharacters, like '.', '^' and '$'."),
+        ("^n.+p$", "noooooooope",     "False",
+            "The repetition operators can be combined with other metacharacters, like '.', '^' and '$'."),
+        ('^.*c$', 'abcabc', "True",
+         "The repetition operators can be combined with other metacharacters, like '.', '^' and '$'.")
         ]
 
-        for test_case in test_cases:
-            self.assertEqual(rx.test_string_with_regex(test_case[0], test_case[1]), test_case[2])
+        for i in range(len(test_cases)):
+            test_case = test_cases[i]
+            print("testing", i + 1, "of", len(test_cases), "\n",test_cases[i])
+            self.assertEqual(str(rx.test_string_with_regex(test_case[0], test_case[1])), test_case[2], test_case[3])
 
 
 if __name__ == '__main__':
